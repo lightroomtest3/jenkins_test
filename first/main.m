6c26bce1a4c3f7dbdfe9d0a500330c197e4de7db
lightroomtest3 <lightroomtest3@gmail.com>
2012-07-10 20:02:16 +0300

    subj
    none

---
+++ first/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  first
+//
+//  Created by tester1 on 1/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool 
+    {
+        //UIButton *get_number_button = [[UIButton alloc]initWithFrame:CGRectMake(10, 10, 50, 50)];
+        //UIButton *get_number_button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        //get_number_button.frame = CGRectMake(100, 170, 100, 30);
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
